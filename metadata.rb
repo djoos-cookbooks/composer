e742e2a9e0f076e236231e45e3c09fbb08344c5d
David Joos <david.joos@gmail.com>
2012-11-27 09:42:21 +0000

    Version 0.0.2

--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "dev@escapestudios.com"
 license          "MIT"
 description      "Installs/Configures Composer"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
+version          "0.0.2"
 
 supports "ubuntu"
 supports "debian"
