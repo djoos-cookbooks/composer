8a6476d3f12aef5fd82203d6a3d982d0465dde08
David Joos <david.joos@gmail.com>
2013-03-13 18:38:41 +0000

    metadata attribute change(s) & copyright update(s)

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: composer
 # Attributes:: default
 #
-# Copyright 2012, Escape Studios
+# Copyright 2012-2013, Escape Studios
 #
 
 default[:composer][:install_globally] = true
--- recipes/default.rb
+++ recipes/default.rb
@@ -2,7 +2,7 @@
 # Cookbook Name:: composer
 # Recipe:: default
 #
-# Copyright 2012, Escape Studios
+# Copyright 2012-2013, Escape Studios
 #
 
 include_recipe "php"
