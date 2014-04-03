70e8ef4aaef5af5ef0e70dd6ef5487393f95d97b
David Joos <david.joos@gmail.com>
2014-04-03 11:27:41 +0100

    Indentation

--- recipes/global_configs.rb
+++ recipes/global_configs.rb
@@ -10,10 +10,11 @@ configs = node['composer']['global_configs']
 unless configs.nil?
     configs.each_pair do |user,user_configs|
         directory "/home/#{user}/.composer" do
-          owner user
-          group user
-          mode 0755
+            owner user
+            group user
+            mode 0755
         end
+
         unless user_configs.nil?
             user_configs.each_pair do |option,value|
                 if value.respond_to?(:each_pair)
