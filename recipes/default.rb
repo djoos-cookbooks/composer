e78d7c7c5defd99cebb67eb4b21d38c9f9191e02
Maks <github.maks3w@virtualplanets.net>
2014-04-28 13:01:31 +0200

    By default install composer
    By default the cookbook should be stable and not to be in the edge

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,7 +5,7 @@
 # Copyright 2012-2014, Escape Studios
 #
 
-include_recipe 'composer::self_update'
+include_recipe 'composer::install'
 
 if node['composer']['install_globally']
   include_recipe 'composer::global_configs'
