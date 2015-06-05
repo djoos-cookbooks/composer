f0a8523eff8a0ca618752c40ab5e27cfc2ca797f
Nielsen Pierce <nielsen.pierce@rackspace.co.uk>
2015-06-05 16:25:55 +0100

    Add require and remove action

--- providers/project.rb
+++ providers/project.rb
@@ -16,6 +16,15 @@ action :install do
   new_resource.updated_by_last_action(true)
 end
 
+# action :single do
+#   require_single_project
+#   install_single_project
+# end
+
+action :require do
+  make_require
+end
+
 action :update do
   make_execute 'update'
   new_resource.updated_by_last_action(true)
@@ -24,6 +33,10 @@ end
 action :dump_autoload do
   make_execute 'dump-autoload'
   new_resource.updated_by_last_action(true)
+end
+
+action :remove do
+  remove_vendor 'remove'
 end
 
 def make_execute(cmd)
@@ -43,6 +56,53 @@ def make_execute(cmd)
     umask new_resource.umask
   end
 end
+
+def make_require
+  dev = new_resource.dev ? '--dev' : '--update-no-dev'
+  vendor = new_resource.vendor
+  prefer_dist = new_resource.prefer_dist ? '--prefer-dist' : ''
+
+  execute 'Install-composer-for-single-project' do
+    cwd new_resource.project_dir
+    command "#{node['composer']['bin']} require #{vendor} #{dev} #{prefer_dist}"
+    environment 'COMPOSER_HOME' => Composer.home_dir(node)
+    action :run
+    only_if 'which composer'
+    user new_resource.user
+    group new_resource.group
+    umask new_resource.umask
+  end
+end
+
+def remove_vendor(cmd)
+  vendor = new_resource.vendor
+
+  execute "#{cmd}-composer-for-project" do
+    cwd new_resource.project_dir
+    command "#{node['composer']['bin']} remove #{vendor}"
+    environment 'COMPOSER_HOME' => Composer.home_dir(node)
+    action :run
+    only_if 'which composer'
+  end
+end
+
+# def install_single_project
+#   path = new_resource.path
+#   vendor = new_resource.vendor
+#   dev = new_resource.dev ? '--dev' : '--no-dev'
+#   prefer_dist = new_resource.prefer_dist ? '--prefer-dist' : ''
+#
+#   execute 'Install-composer-for-project' do
+#     cwd path
+#     command "#{node['composer']['bin']} install #{dev}"
+#     environment 'COMPOSER_HOME' => Composer.home_dir(node)
+#     action :run
+#     only_if 'which composer'
+#     user new_resource.user
+#     group new_resource.group
+#     umask new_resource.umask
+#   end
+# end
 
 def optimize_flag(cmd)
   (%(install update).include? cmd) ? '--optimize-autoloader' : '--optimize'
--- resources/project.rb
+++ resources/project.rb
@@ -5,10 +5,12 @@
 # Copyright 2012-2014, Escape Studios
 #
 
-actions :install, :update, :dump_autoload
+actions :install, :single, :require, :update, :dump_autoload, :remove
 default_action :install
 
 attribute :project_dir, :kind_of => String, :name_attribute => true
+attribute :vendor, :kind_of => String, :name_attribute => true, :required => true
+attribute :path, :kind_of => String, :default => nil
 attribute :dev, :kind_of => [TrueClass, FalseClass], :default => false
 attribute :quiet, :kind_of => [TrueClass, FalseClass], :default => true
 attribute :optimize_autoloader, :kind_of => [TrueClass, FalseClass], :default => false
