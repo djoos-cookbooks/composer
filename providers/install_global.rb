cbf13d3397b5692a544646c222836a69d4073a6d
Dieter Blomme <dieterblomme@gmail.com>
2016-09-29 14:27:25 +0100

    Stash perm

--- providers/install_global.rb
+++ providers/install_global.rb
@@ -27,69 +27,51 @@ action :update do
 end
 
 def install_global_install
-  dev = new_resource.dev ? '--dev' : '--no-dev'
-  quiet = new_resource.quiet ? '--quiet' : ''
-  optimize = new_resource.optimize_autoloader ? optimize_flag(cmd) : ''
+  quiet = '--verbose' #new_resource.quiet ? '--quiet' : ''
+  optimize = new_resource.optimize_autoloader ? '--optimize-autoloader' : ''
   prefer_dist = new_resource.prefer_dist ? '--prefer-dist' : ''
   prefer_source = new_resource.prefer_source ? '--prefer-source' : ''
+  prefer_stable = new_resource.prefer_stable ? '--prefer-stable' : ''
+  package = new_resource.package
 
-  directory new_resource.install_dir do
-    recursive true
-    user new_resource.user
-    group new_resource.group
-  end
-
-  composer_project new_resource.vendor do
-    project_dir new_resource.install_dir
-    dev new_resource.dev
-    quiet new_resource.quiet
-    optimize new_resource.optimize_autoloader
-    prefer_dist new_resource.prefer_dist
-    prefer_source new_resource.prefer_source
-    user new_resource.user
-    group new_resource.group
-    umask new_resource.umask
-    action :require
-  end
-
-  # Set the bin_dir for completeness
-  execute 'set-bin_dir' do
-    cwd new_resource.install_dir
-    command "#{node['composer']['bin']} config bin-dir #{node['composer']['bin_dir']} #{quiet}"
-    environment 'COMPOSER_HOME' => Composer.home_dir(node)
+  execute "composer-install-global-#{package}" do
+    command "#{node['composer']['bin']} global require --no-interaction --no-ansi #{quiet} #{optimize} #{prefer_dist} #{prefer_source} #{prefer_stable} #{package}"
+    environment 'COMPOSER_HOME' => Composer.home_dir(node), 'COMPOSER_BIN_DIR' => Composer.bin_dir(node)
     action :run
     user new_resource.user
     group new_resource.group
     umask new_resource.umask
+    environment "COMPOSER_HOME" => "/home/vagrant"
+    not_if "#{node['composer']['bin']} global show #{new_resource.package}"
   end
-end
 
-def install_global_remove
-  composer_project new_resource.vendor do
-    project_dir new_resource.install_dir
-    dev new_resource.dev
-    quiet new_resource.quiet
-    optimize new_resource.optimize_autoloader
-    prefer_dist new_resource.prefer_dist
-    prefer_source new_resource.prefer_source
-    user new_resource.user
-    group new_resource.group
-    umask new_resource.umask
-    action :remove
-  end
-end
+  # directory new_resource.install_dir do
+  #   recursive true
+  #   user new_resource.user
+  #   group new_resource.group
+  # end
 
-def install_global_update
-  composer_project new_resource.vendor do
-    project_dir new_resource.install_dir
-    dev new_resource.dev
-    quiet new_resource.quiet
-    optimize new_resource.optimize_autoloader
-    prefer_dist new_resource.prefer_dist
-    prefer_source new_resource.prefer_source
-    user new_resource.user
-    group new_resource.group
-    umask new_resource.umask
-    action :update
-  end
+  # composer_project new_resource.vendor do
+  #   project_dir new_resource.install_dir
+  #   dev new_resource.dev
+  #   quiet new_resource.quiet
+  #   optimize new_resource.optimize_autoloader
+  #   prefer_dist new_resource.prefer_dist
+  #   prefer_source new_resource.prefer_source
+  #   user new_resource.user
+  #   group new_resource.group
+  #   umask new_resource.umask
+  #   action :require
+  # end
+
+  # # Set the bin_dir for completeness
+  # execute 'set-bin_dir' do
+  #   cwd new_resource.install_dir
+  #   command "#{node['composer']['bin']} config bin-dir #{node['composer']['bin_dir']} #{quiet}"
+  #   environment 'COMPOSER_HOME' => Composer.home_dir(node)
+  #   action :run
+  #   user new_resource.user
+  #   group new_resource.group
+  #   umask new_resource.umask
+  # end
 end
--- resources/install_global.rb
+++ resources/install_global.rb
@@ -8,14 +8,14 @@
 actions :install, :update, :remove
 default_action :install
 
-attribute :vendor, :kind_of => String, :name_attribute => true, :required => true
-attribute :install_dir, :kind_of => String
+attribute :package, :kind_of => String, :name_attribute => true, :required => true
+attribute :install_dir, :kind_of => String #TODO: what to do with this? Can be driven by COMPOSER_HOME
 attribute :path, :kind_of => String, :default => nil
-attribute :dev, :kind_of => [TrueClass, FalseClass], :default => false
 attribute :quiet, :kind_of => [TrueClass, FalseClass], :default => true
 attribute :optimize_autoloader, :kind_of => [TrueClass, FalseClass], :default => false
 attribute :prefer_dist, :kind_of => [TrueClass, FalseClass], :default => false
 attribute :prefer_source, :kind_of => [TrueClass, FalseClass], :default => false
+attribute :prefer_stable, :kind_of => [TrueClass, FalseClass], :default => false
 attribute :user, :kind_of => String, :default => 'root'
 attribute :group, :kind_of => String, :default => 'root'
 attribute :umask, :kind_of => [String, Fixnum], :default => 0002
