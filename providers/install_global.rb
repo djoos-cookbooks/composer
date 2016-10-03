ce99dfbdea727862deea204a189adef70402ddf9
Dieter Blomme <dieterblomme@gmail.com>
2016-10-03 16:07:27 +0200

    Add global update

--- providers/install_global.rb
+++ providers/install_global.rb
@@ -59,3 +59,20 @@ def install_global_remove
     action :remove
   end
 end
+
+def install_global_update
+  install_dir = new_resource.install_dir ? new_resource.install_dir : node['composer']['global_install']['install_dir']
+  bin_dir = new_resource.bin_dir ? new_resource.bin_dir : node['composer']['global_install']['bin_dir']
+
+  composer_project install_dir do
+    package new_resource.package
+    version new_resource.version
+    bin_dir bin_dir
+    dev new_resource.dev
+    quiet new_resource.quiet
+    optimize_autoloader new_resource.optimize_autoloader
+    prefer_dist new_resource.prefer_dist
+    prefer_source new_resource.prefer_source
+    action :update
+  end
+end
