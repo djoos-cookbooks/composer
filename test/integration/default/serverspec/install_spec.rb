d4040c78e7b0f5829e727b5788f8815810e34cae
Katoga <katoga.cz@hotmail.com>
2017-02-11 12:05:27 +0100

    Add test checking version of composer

--- test/integration/default/serverspec/install_spec.rb
+++ test/integration/default/serverspec/install_spec.rb
@@ -4,3 +4,8 @@ describe file('/usr/local/bin/composer') do
   it { should be_file }
   it { should be_mode 755 }
 end
+
+describe command('/usr/local/bin/composer') do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should contain 'Composer version 1.3.2' }
+end
