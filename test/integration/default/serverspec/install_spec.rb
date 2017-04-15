70a6f84abf66c4ad3ec24bf44a5a684e21810d4f
Katoga <katoga.cz@hotmail.com>
2017-04-15 13:54:28 +0200

    Update composer version to 1.4.1

--- test/integration/default/serverspec/install_spec.rb
+++ test/integration/default/serverspec/install_spec.rb
@@ -4,7 +4,7 @@ set :backend, :exec
 
 # these vars must correspond with attributes' values
 composer_install_path = '/usr/local/bin'
-composer_version = '1.3.2'
+composer_version = '1.4.1'
 
 describe file("#{composer_install_path}/composer") do
   it { should be_file }
